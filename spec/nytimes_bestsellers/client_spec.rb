606961b087f9c408c605a836e94f7a7fc18799ac
Emma Spencer <emma.n.spencer@gmail.com>
2013-11-26 22:56:56 -0500

    changed search_list test to always  test current date

--- spec/nytimes_bestsellers/client_spec.rb
+++ spec/nytimes_bestsellers/client_spec.rb
@@ -24,13 +24,14 @@ describe Bestsellers::List do
 
   describe '#search_list' do
     before do
-      stub_request(:get, "http://api.nytimes.com/svc/books/v2/lists?api-key=abc123&date=2013-11-26&list-name=hardcover-nonfiction").to_return(body: fixture('search_list.json'))
+      @date = Date.today.strftime('%Y-%m-%e')
+      stub_request(:get, "http://api.nytimes.com/svc/books/v2/lists?api-key=abc123&date=#{@date}&list-name=hardcover-nonfiction").to_return(body: fixture('search_list.json'))
     end
 
     it "searches the specified list by optional parameters" do
       list = @client.search_list('hardcover-nonfiction')
       list = JSON.parse(list.body)
-      expect(a_request(:get, "http://api.nytimes.com/svc/books/v2/lists?api-key=abc123&date=2013-11-26&list-name=hardcover-nonfiction")).to have_been_made
+      expect(a_request(:get, "http://api.nytimes.com/svc/books/v2/lists?api-key=abc123&date=#{@date}&list-name=hardcover-nonfiction")).to have_been_made
       expect(list['results'][0]['list_name']).to eq "Hardcover Nonfiction"
     end
 
